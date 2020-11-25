2c55a9ece388be427401a7e319c9e3c9acf6ddfe
barnez299 <barnez29@gmail.com>
2020-11-25 08:25:48 +0200

    templating complete

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-from typing import Optional
-
-from fastapi import FastAPI
+# from typing import Optional
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+# define templates directory
+
+templates = Jinja2Templates(directory="templates")
+
 
 @app.get("/")
-def dashboard():
+def home(request: Request):
     """
     displays dashboard/homepage for stock screener
     """
-    return {"DashBoard": "FastAPI-World"}
+    return templates.TemplateResponse("home.html",{
+        "request": request
+    })
 
 
 @app.post("/stock")
