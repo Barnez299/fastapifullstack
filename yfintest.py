67f57481cab6ccbfea9c7260dafb4698132e4e88
barnez299 <barnez29@gmail.com>
2020-11-26 16:36:03 +0200

    updated template

--- yfintest.py
+++ yfintest.py
@@ -4,3 +4,18 @@ msft = yf.Ticker("MSFT")
 
 # get stock info
 print(msft.info)
+
+
+<script type="text/javascript">
+
+     $(document).ready(function(){
+     $('#coins').click(function(){
+        $('#modalid').modal('show');    
+     });
+     });
+
+     </script>
+     
+     
+     JNJ
+     AAPL
